709572c3f29c7dfd29a7c6c0254b06d84304a0f0
baeseungwon <21storeo@gmail.com>
2023-09-05 23:23:00 +0900

    msg

--- recipe-book/config/production.py
+++ recipe-book/config/production.py
@@ -44,7 +44,7 @@ dictConfig({
         'file': {
             'level': 'INFO',
             'class': 'logging.handlers.RotatingFileHandler',
-            'filename': os.path.join(BASE_DIR, 'logs/myproject.log'),
+            'filename': os.path.join(BASE_DIR, 'logs/ssgrecipe.log'),
             'maxBytes': 1024 * 1024 * 5,  # 5 MB
             'backupCount': 5,
             'formatter': 'default',
