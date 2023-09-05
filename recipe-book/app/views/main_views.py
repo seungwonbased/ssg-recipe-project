385113080e537c8741c74f679511bb3fbc461d8e
baeseungwon <21storeo@gmail.com>
2023-09-05 23:20:29 +0900

    EDIT main_views.py for logging

--- recipe-book/app/views/main_views.py
+++ recipe-book/app/views/main_views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, url_for
+from flask import Blueprint, url_for, current_app
 from werkzeug.utils import redirect
 
 bp = Blueprint('main', __name__, url_prefix='/')
@@ -6,9 +6,11 @@ bp = Blueprint('main', __name__, url_prefix='/')
 
 @bp.route('/hello')
 def hello_pybo():
-    return 'Hello, app!'
+    return 'Hello, recipe!'
 
 
 @bp.route('/')
 def index():
+    current_app.logger.info("Logging level: INFO")
+
     return redirect(url_for('post._list'))
