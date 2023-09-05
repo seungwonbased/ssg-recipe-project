18480d74d28b4bc504d549884b4bf24a7ea05256
baeseungwon <21storeo@gmail.com>
2023-09-05 23:02:47 +0900

    logging

--- recipe-book/config/production.py
+++ recipe-book/config/production.py
@@ -1,6 +1,32 @@
+from logging.config import dictConfig
 from config.default import *
+
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(
     os.path.join(BASE_DIR, 'app.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'\x07\xcfBMsN\x07\A\xab\xc8fkV\xcf4'
+
+
+dictConfig({
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        }
+    },
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/myproject.log'),
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
+            'backupCount': 5,
+            'formatter': 'default',
+        },
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['file']
+    }
+})
