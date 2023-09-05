62a62cfc003570e343ec68f34122852e7c4ebe9b
baeseungwon <21storeo@gmail.com>
2023-09-06 07:54:22 +0900

    EDIT production.py to change database

--- recipe-book/config/production.py
+++ recipe-book/config/production.py
@@ -1,11 +1,16 @@
 from logging.config import dictConfig
 from config.default import *
+from dotenv import load_dotenv
 
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(
-    os.path.join(BASE_DIR, 'app.db'))
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = b'\x07\xcfBMsN\x07\A\xab\xc8fkV\xcf4'
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+
+
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
+    user=os.getenv('DB_USER'),
+    pw=os.getenv('DB_PASSWORD'),
+    url=os.getenv('DB_HOST'),
+    db=os.getenv('DB_NAME'))
 
 """
 - dictConfig
