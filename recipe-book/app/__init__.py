b462e454a177f3b778a5c55c8f8e2fb967fd8949
baeseungwon <21storeo@gmail.com>
2023-09-05 22:54:43 +0900

    msg

--- recipe-book/app/__init__.py
+++ recipe-book/app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
@@ -16,8 +16,14 @@ naming_convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
     "pk": "pk_%(table_name)s"
 }
+
+
 db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
 migrate = Migrate()
+
+
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 def create_app():
@@ -47,5 +53,8 @@ def create_app():
 
     # Markdown
     Markdown(app, extensions=['nl2br', 'fenced_code'])
+
+    # error page
+    app.register_error_handler(404, page_not_found)
 
     return app
