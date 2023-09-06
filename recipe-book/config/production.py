692aac3485698096a90ac4b142aa2b8fb0bd8e5a
baeseungwon <21storeo@gmail.com>
2023-09-06 09:09:40 +0900

    ADD SECRET_KEY

--- recipe-book/config/production.py
+++ recipe-book/config/production.py
@@ -11,6 +11,9 @@ SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
     pw=os.getenv('DB_PASSWORD'),
     url=os.getenv('DB_HOST'),
     db=os.getenv('DB_NAME'))
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = b'Zb3\x81\xdb\xf1\xd9\xd7-Knb\x8eB\xa5\x18'
+
 
 """
 - dictConfig
