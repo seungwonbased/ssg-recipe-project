3c134a6b0eb5cacad6ac2534540d357cfc78f241
baeseungwon <21storeo@gmail.com>
2023-09-06 23:30:52 +0900

    msg

--- recipe-book/app/__init__.py
+++ recipe-book/app/__init__.py
@@ -3,15 +3,16 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from flaskext.markdown import Markdown
+
 # from kamis import kamis_api
 
 
 naming_convention = {
-    "ix": 'ix_%(column_0_label)s',
+    "ix": "ix_%(column_0_label)s",
     "uq": "uq_%(table_name)s_%(column_0_name)s",
     "ck": "ck_%(table_name)s_%(column_0_name)s",
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    "pk": "pk_%(table_name)s"
+    "pk": "pk_%(table_name)s",
 }
 
 
@@ -20,37 +21,39 @@ migrate = Migrate()
 
 
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template("404.html"), 404
 
 
+# Application Factory Function
 def create_app():
     app = Flask(__name__)
-    app.config.from_envvar('APP_CONFIG_FILE')
+    app.config.from_envvar("APP_CONFIG_FILE")
 
-    # ORM
     db.init_app(app)
-    if app.config['SQLALCHEMY_DATABASE_URI'].startswith("sqlite"):
+    if app.config["SQLALCHEMY_DATABASE_URI"].startswith("sqlite"):
         migrate.init_app(app, db, render_as_batch=True)
     else:
         migrate.init_app(app, db)
 
     # from . import models
 
-    # 블루 프린트
+    # Blueprints
     from .views import main_views, post_views, comment_views, auth_views
+
     app.register_blueprint(main_views.bp)
     app.register_blueprint(post_views.bp)
     app.register_blueprint(comment_views.bp)
     app.register_blueprint(auth_views.bp)
 
-    # 필터
+    # Filter
     from .filter import format_datetime
-    app.jinja_env.filters['datetime'] = format_datetime
+
+    app.jinja_env.filters["datetime"] = format_datetime
 
     # Markdown
-    Markdown(app, extensions=['nl2br', 'fenced_code'])
+    Markdown(app, extensions=["nl2br", "fenced_code"])
 
-    # error page
+    # Error Page
     app.register_error_handler(404, page_not_found)
 
     return app
--- recipe-book/app/filter.py
+++ recipe-book/app/filter.py
@@ -1,6 +1,8 @@
 import locale
-locale.setlocale(locale.LC_ALL, '')
+
+locale.setlocale(locale.LC_ALL, "")
 
 
-def format_datetime(value, fmt='%Y년 %m월 %d일 %p %I:%M'):
+# datetime 객체를 문자열로 만들어주는 Template Filter
+def format_datetime(value, fmt="%Y년 %m월 %d일 %p %I:%M"):
     return value.strftime(fmt)
